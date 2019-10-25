1ff34da696306902ed4623ace91300c6179bcd21
shuheydev <shuhey.dev@gmail.com>
2019-10-25 23:57:08 +0900

    Add project files.

---
+++ XFDesignData2/XFDesignData2/Models/People.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XFDesignData2.Models
+{
+    public class People : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (_age == value) return;
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _image;
+        public string Image
+        {
+            get => _image;
+            set
+            {
+                if (_image == value) return;
+                _image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ XFDesignData2/XFDesignData2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XFDesignData2.Models
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public bool IsBusy { get; set; }
+
+        private ObservableCollection<People> _members;
+        public ObservableCollection<People> Members
+        {
+            get => _members;
+            set
+            {
+                if (_members == value) return;
+                _members = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
